45c01dbe5f9f3ca2d5a226d48ee588437db41cfd
David <tr.ywchiu@gmail.com>
2016-07-28 16:36:56 +0800

    add Library

---
+++ Lib/TermDocumentMatrixCN.R
@@ -0,0 +1,21 @@
+
+library(jiebaR)
+library(tm)
+DocumentTermMatrixCN = function(d.vec){
+  mixseg = worker()
+  
+  jieba_tokenizer=function(d){
+    unlist(segment(d[[1]],mixseg))
+  }
+  
+  space_tokenizer=function(x){
+    unlist(strsplit(as.character(x[[1]]),'[[:space:]]+'))
+  }
+  doc=VCorpus(VectorSource(d.vec ))
+  doc=unlist(tm_map(doc,jieba_tokenizer),recursive=F)
+  doc=lapply(doc,function(d)paste(d,collapse=' '))
+  
+  control.list=list(wordLengths=c(1,Inf),tokenize=space_tokenizer)
+  dtm=DocumentTermMatrix(Corpus(VectorSource(doc)),control=control.list)
+  dtm
+}
